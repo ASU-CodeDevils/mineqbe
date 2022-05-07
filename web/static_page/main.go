18776cd4a9e156caa6c53508785a21b48e1507d8
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2022-05-06 21:15:57 -0700

    Create main.go

---
+++ web/static_page/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func newRouter() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/hello", handler).Methods("GET")
+
+	staticFileDirectory := http.Dir("./assets/")
+	staticFileHandler := http.StripPrefix("/assets/", http.FileServer(staticFileDirectory))
+	r.PathPrefix("/assets/").Handler(staticFileHandler).Methods("GET")
+	return r
+}
+
+func main() {
+	r := newRouter()
+	http.ListenAndServe(":8080", r)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+}
